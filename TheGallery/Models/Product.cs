96c23d969da0976bb29852ee19eb56a8a7c4a3a5
martinbacs <martinbackstroms@hotmail.com>
2019-05-20 20:13:27 +0200

    added image to Products Views: Index and Details

--- TheGallery/Models/Product.cs
+++ TheGallery/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,6 +22,7 @@ namespace TheGallery.Models
         public string Name { get; set; }
         [Required]
         public string ImageURL { get; set; }
+
         public string Information { get; set; }
 
         public virtual List<CartItem> CartItems { get; set; }
